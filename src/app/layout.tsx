9c69d5790cb8bde0f70d6f129fca0cf73142c00f
Tomasz Chybziński <tomasz.chybzinski@gmail.com>
2023-11-29 12:43:48 +0100

    chore: poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import '@/app/normalize.css';
 import '@/app/globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 export default function Home() {
   const x = 'name';
 
-  return <main>Home</main>;
+  return (
+    <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores obcaecati
+      possimus dolorum consequatur nemo culpa ipsum quam sapiente quis
+      asperiores rem cumque ad id voluptate illum temporibus, quibusdam optio
+      animi vel repellat, voluptas eius iure perferendis vero. A odit optio
+      tempora, facere dolores, impedit quidem distinctio voluptate consectetur
+      ullam ipsa assumenda excepturi quis aliquid culpa! Aliquam ut amet nam.
+      Vitae, fugit molestiae quisquam illum, doloremque provident harum nam
+      rerum deleniti dolor atque vel commodi unde autem aperiam veritatis. Rerum
+      at eaque, magni reprehenderit sit in ab, itaque, earum aperiam tempore
+      officia nulla sapiente illo consectetur velit porro omnis aliquam veniam.
+    </main>
+  );
 }
