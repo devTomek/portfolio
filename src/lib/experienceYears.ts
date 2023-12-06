910221460634af619d157c54c4dab2628d067705
Tomasz Chybziński <tomasz.chybzinski@gmail.com>
2023-12-06 12:59:20 +0100

    fix: include month and day in experience years

--- src/lib/experienceYears.ts
+++ src/lib/experienceYears.ts
@@ -1,3 +1,8 @@
-const careerStartedYear = new Date(2017, 7, 1).getFullYear();
-const currentyear = new Date().getFullYear();
-export const experienceYears = currentyear - careerStartedYear;
+const careerStartedDate = new Date(2017, 7, 1);
+const currentDate = new Date();
+const diff = +currentDate - +careerStartedDate;
+
+// 365.25 because the average year is 365.25 days long. Every 4 years, we add a day to account for leap years.
+export const experienceYears = Math.floor(
+  diff / (1000 * 60 * 60 * 24 * 365.25),
+);
