607c3c6e616fc2d73be7e1f07da9f28e563df6f1
Tomasz Chybziński <tomasz.chybzinski@gmail.com>
2023-12-15 14:09:58 +0100

    chore: dynamic footer year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="h-[50px] flex items-center justify-center">
-      <p>Copyright 2023 | Tomasz Chybziński</p>
+      <p>Copyright {new Date().getFullYear()} | Tomasz Chybziński</p>
     </footer>
   );
 }
